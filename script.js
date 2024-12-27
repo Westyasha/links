66c8a807eca395a7795f3e2355c59a19390e2ca4
Westyasha <vladflaik@gmail.com>
2024-12-27 04:23:31 +0300

    повторки

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const linksTextarea = document.getElementById('links');
 const resultDiv = document.getElementById('result');
+const duplicatesDiv = document.getElementById('duplicates');
+const duplicatesToggle = document.getElementById('duplicates-toggle');
 const container = document.querySelector('.container');
 
 function updateResult() {
@@ -7,19 +9,57 @@ function updateResult() {
     const links = text.match(/https:\/\/\S+/g) || [];
     const linksCount = links.length;
 
+    let uniqueLinksCount = linksCount;
+    let duplicateLinksCount = 0;
+
+    if (duplicatesToggle.checked) {
+        const duplicates = findDuplicates(links);
+        duplicateLinksCount = duplicates.length;
+        uniqueLinksCount = linksCount - duplicateLinksCount;
+
+        if (duplicates.length > 0) {
+            duplicatesDiv.innerHTML = 'Повторные ссылки:<br>' + duplicates.join('<br>');
+            duplicatesDiv.classList.add('show');
+        } else {
+            duplicatesDiv.innerHTML = '';
+            duplicatesDiv.classList.remove('show');
+        }
+    } else {
+        duplicatesDiv.innerHTML = '';
+        duplicatesDiv.classList.remove('show');
+    }
+
     let highlightClass = '';
-    if (linksCount < 20) {
+    if (uniqueLinksCount < 20) {
         highlightClass = 'negative';
-    } else if (linksCount >= 20 && linksCount < 30) {
+    } else if (uniqueLinksCount >= 20 && uniqueLinksCount < 30) {
         highlightClass = 'low';
     } else {
         highlightClass = 'positive';
     }
 
-    resultDiv.innerHTML = `<span class="result-text">Количество ссылок: </span><span class="highlight ${highlightClass}"><span class="count">${linksCount}</span></span>`;
+    resultDiv.innerHTML = `<span class="result-text">Количество ссылок: </span><span class="highlight ${highlightClass}"><span class="count">${uniqueLinksCount}</span></span>`;
+
+    if (duplicatesToggle.checked) {
+        resultDiv.innerHTML += ` | <span class="highlight duplicate"><span class="count">${duplicateLinksCount}</span></span>`;
+    }
+}
+
+function findDuplicates(arr) {
+    const seen = new Set();
+    const duplicates = new Set();
+    for (const item of arr) {
+        if (seen.has(item)) {
+            duplicates.add(item);
+        } else {
+            seen.add(item);
+        }
+    }
+    return Array.from(duplicates);
 }
 
 linksTextarea.addEventListener('input', updateResult);
+duplicatesToggle.addEventListener('change', updateResult);
 
 const wrapper = document.querySelector('.wrapper');
 
